7882f0a7b5ffdd51730b325921ff7e6e475f8ce2
Neriya Malachi <neriamalachi@gmail.com>
2024-06-16 15:38:55 +0300

    feat
    start  connect to mongodb open db and between connect them

---
+++ my-app/api/conectToMongodb/route.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+// global.mongoose ={
+    
+// }
+
+export async function dbConnect (){
+    
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 import Image from "next/image";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Home() {
-  const [name, setName] = useState('');
-  const [gender, setGender] = useState('');
-  const [nationality, setNationality] = useState('');
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [nationality, setNationality] = useState("");
 
-  const handleSubmit = async (e:any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
 
     // בקשה ל-genderize.io
@@ -15,33 +15,40 @@ export default function Home() {
     const genderData = await genderResponse.json();
 
     // בקשה ל-nationalize.io
-    const nationalityResponse = await fetch(`https://api.nationalize.io?name=${name}`);
+    const nationalityResponse = await fetch(
+      `https://api.nationalize.io?name=${name}`
+    );
     const nationalityData = await nationalityResponse.json();
 
     // עדכון המצב עם הנתונים שהתקבלו
     setGender(genderData.gender);
-    setNationality(nationalityData.country.map((country:any) => country.country_id).join(', '));
+    setNationality(
+      nationalityData.country
+        .map((country: any) => country.country_id)
+        .join(", ")
+    );
   };
   return (
-
-    
-        <div className="bg-slate-500 flex flex-col justify-evenly items-center  w-full h-[100vh] text-black">
-          <form onSubmit={handleSubmit} className="flex flex-col">
-            <label>
-              Enter a name:
-              <input 
-                type="text" 
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="text-black"
-              />
-            </label>
-            <button type="submit" className="bg-yellow-100 rounded-md">Predict</button>
-          </form>
-          {gender && <p>Gender: {gender}</p>}
-          {nationality && <p>Nationality: {nationality}</p>}
-        </div>
-    
-    
+    <div className="bg-slate-200 flex flex-col justify-evenly items-center  w-full h-[100vh] text-black">
+      <form
+        onSubmit={handleSubmit}
+        className=" w-96 h-96  flex flex-col justify-evenly items-center bg-gray-50 rounded-md shadow-2xl"
+      >
+        <label className=" flex flex-col justify-around ">
+          Enter a name :
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="text-black border border-black rounded-md"
+          />
+          <p>Gender : {gender && <> {gender}</>}</p>
+          <p> Nationality : {nationality && <> {nationality}</>}</p>
+        </label>
+        <button type="submit" className="bg-yellow-200 w-[50%]  rounded-md">
+          Predict
+        </button>
+      </form>
+    </div>
   );
 }
